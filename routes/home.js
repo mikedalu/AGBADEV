481855b395f17c9f089393e8d075a8af4058eade
mikedalu <allegrotech89@gmail.com>
2022-08-24 11:13:15 +0100

    Added an ejs template for showing home endpoint route

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //HOME ROUTE
 router.get("/", (req, res) => {
-	res.status(200).send(`<h1>Welcome to AGBADEV </h1>`);
+	res.render("welcome");
 });
 
 module.exports = router;
